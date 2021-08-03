7dc23a0e3d4f176061064e9e5258f74ac59c1c0c
Aurélien <aurelien.chappard@deefuse.fr>
2021-08-03 08:51:17 +0200

    Add vue-router for view-navigation

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <template>
   <h1>Réveil de Rose</h1>
-  <p class="text-muted fs-7 no-select">Aucune adresse ip n’est configurée pour le réveil.</p>
-  <IpField placeholder @changed="ipChanged"/>
-  <div id="valid-ip-btn-wrapper">
-    <ButtonOrange>Enregister</ButtonOrange>
+  <div id="nav">
+    <router-link :to="{name:'Home'}">Home</router-link> |
+    <router-link :to="{name:'IpSettings'}">Ip Setting</router-link>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import IpField from "./components/form/IpField";
-import {ref} from "vue";
-import ButtonOrange from "./components/form/ButtonOrange";
-
 export default {
   name: 'App',
-  components: {
-    ButtonOrange,
-    IpField
-  },
-  setup() {
-    const ipIsValid = ref(false)
-    const ipChanged = (event) => {
-      ipIsValid.value = event.is_valid;
-      // console.log("ip reçue : " + event.ip + ", port:" + event.port + "  =>  " + (event.is_valid ? "Cette ip est valide" : "Cette Ip n'est pas valide") );
-      // console.log("ip formated = "+event.ip_formatted);
-    }
-    return {ipChanged, ipIsValid}
-  },
 }
 </script>
 
---
+++ src/views/SettingIp.vue
@@ -0,0 +1,30 @@
+<template>
+    <p class="text-muted fs-7 no-select">Aucune adresse ip n’est configurée pour le réveil.</p>
+  <IpField placeholder @changed="ipChanged"/>
+  <div id="valid-ip-btn-wrapper">
+    <ButtonOrange>Enregister</ButtonOrange>
+  </div>
+</template>
+
+<script>
+import ButtonOrange from "../components/form/ButtonOrange";
+import IpField from "../components/form/IpField";
+import {ref} from "vue";
+
+export default {
+  name: 'App',
+  components: {
+    ButtonOrange,
+    IpField
+  },
+  setup() {
+    const ipIsValid = ref(false)
+    const ipChanged = (event) => {
+      ipIsValid.value = event.is_valid;
+      // console.log("ip reçue : " + event.ip + ", port:" + event.port + "  =>  " + (event.is_valid ? "Cette ip est valide" : "Cette Ip n'est pas valide") );
+      // console.log("ip formated = "+event.ip_formatted);
+    }
+    return {ipChanged, ipIsValid}
+  },
+}
+</script>
