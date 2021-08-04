dffd1699ce63948331ad429a75926fcdae173c49
Aurélien <aurelien.chappard@deefuse.fr>
2021-08-04 13:53:19 +0200

    🍔Add burger menu

---
+++ src/components/BurgerIcon.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="burger-menu">
+    <input type="checkbox" />
+    <span class="first"></span>
+    <span class="middle"></span>
+    <span class="last"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BurgerIcon"
+}
+</script>
+
+<style lang="scss" scoped>
+#burger-menu{
+  position: absolute;
+  width: $width-close-menu;
+  height: $width-close-menu;
+  overflow: hidden;
+  top: 0;
+  right: 0;
+  user-select: none;
+  cursor: pointer;
+  padding-top: 12px;
+  input{
+    display: block;
+    width: $width-close-menu;
+    height: $width-close-menu;
+    position: absolute;
+    top:0px;
+    left: 0px;
+    cursor: pointer;
+    border-radius: 0;
+    border: none;
+
+    opacity: 0; /* hide this */
+    z-index: 2;
+    -webkit-touch-callout: none;
+    &:checked ~ span{
+      opacity: 1;
+      transform: rotate(45deg) translate(2px, 0px);
+
+      &.middle{
+        opacity: 0;
+        transform: rotate(0deg) scale(0.2, 0.2);
+      }
+      &.last{
+        transform: rotate(-45deg) translate(0, 2px);
+      }
+    }
+  }
+  span{
+    display: block;
+    width: 15px;
+    height: 2px;
+    margin-bottom: 2px;
+    margin-left: 10.5px;// ($width-close-menu - 14px) / 2;
+    position: relative;
+    background: $white;
+    border-radius: 3px;
+    z-index: 1;
+    transform-origin: 4px 0;
+    transition: transform .5s cubic-bezier(0.77,0.2,0.05,1.0),
+    background .5s cubic-bezier(0.77,0.2,0.05,1.0),
+    opacity .55s ease;
+
+    &.top{
+      transform-origin: 0 100%;
+    }
+    &.last{
+      transform-origin: 0 0;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
   <DynamicBackground>
     <h1>Réveil de Rose</h1>
+    <aside id="clock-settings">
+      <BurgerIcon/>
+    </aside>
     <ul class="list-unstyled">
       <li>Ip Clock = {{ ipClock }}</li>
     </ul>
-    
   </DynamicBackground>
 </template>
 <script>
@@ -17,22 +19,25 @@ import {
 } from "../store/mutation-types";
 import {IP_CLOCK_MODULE, PERIOD_CLOCK_MODULE} from "../store/namespaces";
 import DynamicBackground from "../components/DynamicBackground";
+import BurgerIcon from "../components/BurgerIcon";
+
 export default {
   name: "Home",
-  components:{
-   DynamicBackground,
+  components: {
+    DynamicBackground,
+    BurgerIcon,
   },
-  methods : {
+  methods: {
     ...mapActions([
-        `${PERIOD_CLOCK_MODULE}${SET_IS_BED_TIME}`,
-        `${PERIOD_CLOCK_MODULE}${SET_IS_DAY_TIME}`,
+      `${PERIOD_CLOCK_MODULE}${SET_IS_BED_TIME}`,
+      `${PERIOD_CLOCK_MODULE}${SET_IS_DAY_TIME}`,
     ]),
   },
-  computed:{
+  computed: {
     ...mapGetters({
-      ipClock: `${IP_CLOCK_MODULE}${GET_IP_CLOCK}`,
-      isBedTime: `${PERIOD_CLOCK_MODULE}${GET_IS_BED_TIME}`,
-    }
+          ipClock: `${IP_CLOCK_MODULE}${GET_IP_CLOCK}`,
+          isBedTime: `${PERIOD_CLOCK_MODULE}${GET_IS_BED_TIME}`,
+        }
     ),
   },
 }
